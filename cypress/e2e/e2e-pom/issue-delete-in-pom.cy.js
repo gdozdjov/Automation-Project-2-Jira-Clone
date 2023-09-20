288947342c85acdd24aede7b58e636c943687402
gdozdjov <german.dozdjov@gmail.com>
2023-09-20 08:56:23 +0300

    Wrote tests using POM

--- cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
+++ cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
@@ -1,25 +1,25 @@
-/**
- * This is an example file and approach for POM in Cypress
- */
 import IssueModal from "../../pages/IssueModal";
 
-describe('Issue delete', () => {
+describe('Issue deletion using POM', () => {
   beforeEach(() => {
     cy.visit('/');
     cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`).then((url) => {
-    //open issue detail modal with title from line 16  
     cy.contains(issueTitle).click();
     });
   });
 
-  //issue title, that we are testing with, saved into variable
   const issueTitle = 'This is an issue of type: Task.';
 
-  it('Should delete issue successfully', () => {
-    //add steps to delete issue
+  it('Verify that the first task can be deleted', () => {
+    IssueModal.clickDeleteButton();
+    IssueModal.confirmDeletion();
+    IssueModal.ensureIssueIsNotVisibleOnBoard(issueTitle);
   });
 
-  it('Should cancel deletion process successfully', () => {
-    //add steps to start deletion proces but cancel it
+  it('Verify that it is possible to cancel the deletion of a task', () => {
+    IssueModal.clickDeleteButton();
+    IssueModal.cancelDeletion();
+    IssueModal.closeDetailModal();
+    IssueModal.validateIssueVisibilityState(issueTitle, true)
   });
 });
